030580f434905f3d891a46ad9311e4b189dc437f
Joshua Waheed <joshua.waheed@gmail.com>
2023-03-03 12:41:45 +0000

    Create the sign up activity

--- Projemanag/app/src/main/java/com/joshuawaheed/projemanag/IntroActivity.kt
+++ Projemanag/app/src/main/java/com/joshuawaheed/projemanag/IntroActivity.kt
@@ -1,8 +1,10 @@
 package com.joshuawaheed.projemanag
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,9 @@ class IntroActivity : AppCompatActivity() {
         setContentView(R.layout.activity_intro)
         val flagFullScreen = WindowManager.LayoutParams.FLAG_FULLSCREEN
         window.setFlags(flagFullScreen, flagFullScreen)
+        val btnSignUp: Button = findViewById(R.id.btn_sign_up_intro)
+        btnSignUp.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, SignUpActivity::class.java))
+        }
     }
 }
