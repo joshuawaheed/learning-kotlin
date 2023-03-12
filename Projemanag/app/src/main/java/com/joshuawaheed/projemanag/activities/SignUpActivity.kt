cdbabb93d7fea0a1f90374af4f0795800b5e9c69
Joshua Waheed <joshua.waheed@gmail.com>
2023-03-12 08:56:02 +0000

    Add sign up validation and google-services.json

--- Projemanag/app/src/main/java/com/joshuawaheed/projemanag/activities/SignUpActivity.kt
+++ Projemanag/app/src/main/java/com/joshuawaheed/projemanag/activities/SignUpActivity.kt
@@ -2,11 +2,15 @@ package com.joshuawaheed.projemanag.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.joshuawaheed.projemanag.R
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -29,6 +33,51 @@ class SignUpActivity : AppCompatActivity() {
 
         toolbar.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
+        }
+
+        val btnSignUp: Button = findViewById(R.id.btn_sign_up)
+
+        btnSignUp.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser() {
+        val etName: EditText = findViewById(R.id.et_name)
+        val name: String = etName.text.toString().trim { it <= ' ' }
+
+        val etEmail: EditText = findViewById(R.id.et_email)
+        val email: String = etEmail.text.toString().trim { it <= ' ' }
+
+        val etPassword: EditText = findViewById(R.id.et_password)
+        val password: String = etPassword.text.toString().trim { it <= ' ' }
+
+        if (validateForm(email, name, password)) {
+            Toast.makeText(
+                this@SignUpActivity,
+                "Now we can register a new user",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun validateForm(name: String, email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(name) -> {
+                showErrorSnackBar("Please enter a name")
+                false
+            }
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter an email address")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter a password")
+                false
+            }
+            else -> {
+                true
+            }
         }
     }
 }
